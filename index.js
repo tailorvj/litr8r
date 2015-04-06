e44c27ce3ee3012498dbd70753783acc728ba552
Tailor Vijay <tailor.vj@gmail.com>
2015-04-06 17:47:09 +0300

    changed listen command to support heroku dynamic port assignment

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ io.on('connection', function(socket){
   });
 });
 
-
-http.listen(5000, function(){
+//.listen(process.env.PORT || 5000)
+http.listen(process.env.PORT || 5000, function(){
   console.log('listening on *:5000');
 });
